2812aa6968dc927088f4264941cec927ce442991
jinnu-dev <jinwoolian@gmail.com>
2024-09-14 23:20:55 +0900

    Create __init__.py

---
+++ Melo-City-Release-master/event/__init__.py
@@ -0,0 +1,8 @@
+import event.on_ready
+import event.on_member_join
+import event.on_command_error
+import event.on_message
+import event.on_message_delete
+import event.on_voice_state_update
+import event.on_reaction_add
+import event.on_invite_create
